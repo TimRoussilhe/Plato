fb2bec44a19104819777adf59e736fbef2edb78c
Tim Roussilhe <timothee.roussilhe@gmail.com>
2018-12-21 22:32:32 -0500

    lighthouse fix

--- site-config.js
+++ site-config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-	siteTitle: 'Dipsea',
-	siteTitleShort: 'Dipsea Stories',
-	siteDescription: 'Sexy audio stories that set the mood and spark your imagination',
-	siteUrl: 'https://www.dipseastories.com',
+	siteTitle: 'Plato',
+	siteTitleShort: 'Plato Plato',
+	siteDescription: 'Super Duper Static Site generator',
+	siteUrl: 'https://www.plato.com',
 	themeColor: '#000',
 	backgroundColor: '#fff',
 	pathPrefix: null,
