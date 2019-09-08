b546dcbd5a6c9867b97a8bf693e33011e0fc5b59
Tim Roussilhe <timothee.roussilhe@gmail.com>
2019-09-08 11:12:48 -0400

    Remove helpers folder to use only utils

--- src/scripts/app/utils/cleanURL.js
+++ src/scripts/app/utils/cleanURL.js
@@ -1,9 +1,9 @@
 const expr = /\/+$/;
 
-const removeTrailingSlash = (str) =>{
+const removeTrailingSlash = (str) => {
 	return String(str).replace(expr, '');
 };
-const removeTrailingIndexHTML = (str) =>{
+const removeTrailingIndexHTML = (str) => {
 	return str.split('index.html')[0];
 };
 
@@ -15,8 +15,8 @@ export const cleanURL = (url, options = {}) => {
 	// const testUrl = removeTrailingSlash(url);
 	const testNoIndex = removeTrailingIndexHTML(url);
 
-	if (testNoIndex !== url){
-		window.history.replaceState( null, null, testNoIndex );
+	if (testNoIndex !== url) {
+		window.history.replaceState(null, null, testNoIndex);
 	}
 
 };
