cf8b0120e83b82e4d2326b120d56c2f19bee6e81
Tim Roussilhe <timothee.roussilhe@gmail.com>
2020-05-24 16:34:43 -0400

    Update Utils

--- src/scripts/app/utils/misc.js
+++ src/scripts/app/utils/misc.js
@@ -1,7 +1,3 @@
-/* global Modernizr */
-// import Detectizr from 'detectizr';
-// import Modernizr from 'Modernizr';
-
 import { VIDEO_EXTENSIONS, IMAGE_EXTENSIONS, VIDEO_TYPE, IMAGE_TYPE, FILE_TYPE } from 'constants/misc';
 
 export const getExtension = src => {
@@ -264,6 +260,30 @@ export const fitAsset = config_ => {
 	return newDimensions;
 };
 
+export const fitAssetContains = config_ => {
+	const wi = config_.width;
+	const hi = config_.height;
+	const ri = wi / hi;
+	const ws = config_.containerWidth;
+	const hs = config_.containerHeight;
+	const rs = ws / hs;
+	const newDimensions = {};
+
+	// item is wider than container
+	if (ri > rs) {
+		newDimensions.ratio = hs / hi;
+		newDimensions.w = ws;
+		newDimensions.h = Math.ceil(ws / ri);
+	}
+	// item is taller than container
+	else {
+		newDimensions.ratio = ws / wi;
+		newDimensions.w = Math.ceil(hs * ri);
+		newDimensions.h = hs;
+	}
+	return newDimensions;
+};
+
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
 // N milliseconds. If `immediate` is passed, trigger the function on the
@@ -281,4 +301,32 @@ export const debounce = (func, wait, immediate) => {
 		timeout = setTimeout(later, wait);
 		if (callNow) func.apply(this, args);
 	};
+};
+
+export const throttle = (callback, wait, immediate = false) => {
+	let timeout = null;
+	let initialCall = true;
+
+	return function() {
+		const callNow = immediate && initialCall;
+		const next = () => {
+			callback.apply(this, arguments);
+			timeout = null;
+		};
+
+		if (callNow) {
+			initialCall = false;
+			next();
+		}
+
+		if (!timeout) {
+			timeout = setTimeout(next, wait);
+		}
+	};
+};
+
+export const getRandomIntInclusive = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
