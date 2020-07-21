31f540eaac5801fe5fd01e4c3a3d7609f720632b
Tim Roussilhe <timothee.roussilhe@gmail.com>
2020-07-21 17:20:45 -0400

    Add isMobileViewport to global selector

--- src/scripts/app/selectors/global.js
+++ src/scripts/app/selectors/global.js
@@ -1,1 +1,6 @@
+import store from 'store';
 // global selector go here
+export const isMobileViewport = () => {
+	const browser = store.getState().browser;
+	return browser.is.mobile || browser.is.tablet;
+};
